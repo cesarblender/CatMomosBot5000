110c58a51e21b50f52d5da3accedad55f54b42b9
seokkuuu <seokkub@gmail.com>
2023-08-19 15:00:47 -0400

    Added BigScience/Bloom AI model to generate posts (#1)

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import random
 import time
 import facebook
 import os
+from src.ai.bloom import gen_boomer_text
 
 from src.sources import cat_urls
 
 # Configuración de la API de Facebook
-token = os.getenv("token")
+token = os.getenv("TOKEN")
 graph = facebook.GraphAPI(token)
 
 # Función para obtener una imagen aleatoria de gato de CATAAS
@@ -20,9 +21,12 @@ def get_random_cat_url():
 
 # Función para publicar una foto de gato en Facebook
 def post_cat_photo():
+    text, params = gen_boomer_text()
     cat_url = get_random_cat_url()
     img_data = requests.get(cat_url).content
-    graph.put_photo(image=img_data, message="CatMomo")
+    graph.put_photo(image=img_data, message="""{}\n\n⚠ este texto fue generado usando inteligencia artificial. en caso de que el texto sea ofensivo, o extraño, pueden comentar !report para que sea eliminada la publicacion.\n\nℹ Informacion del modelo:\n
+Modelo: Bloom\n
+foto: API externa\n{}""".format(text, params))
     print("Publicación realizada en Facebook a las ", time.strftime("%H:%M:%S"))
 
 # Bucle principal del bot que publica una foto de gato cada 3 horas
--- src/sources.py
+++ src/sources.py
@@ -1,6 +1,5 @@
 cat_urls = [
-    "https://cataas.com/cat/cute/says/random%20cat%20bot",
-    "https://cataas.com/cat/says/random%20cat%20bot",
-    "https://cataas.com/cat/says/random%20cat%20bot?filter=pixel",
+    "https://cataas.com/cat",
+    "https://cataas.com/cat?filter=pixel",
     "https://picsum.photos/64/64"
 ]
