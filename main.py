cad4198bbf848b80f2367a81ce5ba95063df8c18
seokkuuu <seokkub@gmail.com>
2023-08-19 15:25:55 -0400

    Refactorizing main function and settings file

--- main.py
+++ main.py
@@ -2,34 +2,8 @@ import dotenv
 
 dotenv.load_dotenv()
 
-import requests
-import random
-import time
-import facebook
-import os
-from src.ai.bloom import gen_boomer_text
+def main():
+    pass
 
-from src.sources import cat_urls
-
-# Configuración de la API de Facebook
-token = os.getenv("TOKEN")
-graph = facebook.GraphAPI(token)
-
-# Función para obtener una imagen aleatoria de gato de CATAAS
-def get_random_cat_url():
-    return random.choice(cat_urls)
-
-# Función para publicar una foto de gato en Facebook
-def post_cat_photo():
-    text, params = gen_boomer_text()
-    cat_url = get_random_cat_url()
-    img_data = requests.get(cat_url).content
-    graph.put_photo(image=img_data, message="""{}\n\n⚠ este texto fue generado usando inteligencia artificial. en caso de que el texto sea ofensivo, o extraño, pueden comentar !report para que sea eliminada la publicacion.\n\nℹ Informacion del modelo:\n
-Modelo: Bloom\n
-foto: API externa\n{}""".format(text, params))
-    print("Publicación realizada en Facebook a las ", time.strftime("%H:%M:%S"))
-
-# Bucle principal del bot que publica una foto de gato cada 3 horas
-while True:
-    post_cat_photo()
-    time.sleep(120*3)  # 3 horas
+if __name__ == "__main__":
+    main()
--- src/settings.py
+++ src/settings.py
@@ -1,15 +1,1 @@
-import os
-import json
-from firebase_admin import credentials
-import firebase_admin
-
-firebase_credentials_json = os.environ.get("FIREBASE_CREDENTIALS")
-
-# Convertir la cadena JSON en un objeto Python
-firebase_credentials_dict = json.loads(firebase_credentials_json)
-
-# Crear el objeto de credenciales
-cred = credentials.Certificate(firebase_credentials_dict)
-
-# Inicializar la aplicación de Firebase con las credenciales
-firebase_admin.initialize_app(cred, {'storageBucket': os.environ.get("STORAGE_LINK")})
+BOT_POST_FREQUENCY = 120 * 3 # Each 3 hours
