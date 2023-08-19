25977cef24730aaa1e7b302e81d785b93d9b62f6
seokkuuu <seokkub@gmail.com>
2023-08-19 16:40:54 -0400

    Added env variables into settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,1 +1,8 @@
+import os
+
 BOT_POST_FREQUENCY = 120 * 3 # Each 3 hours
+
+FB_SDK_TOKEN = os.getenv("TOKEN")
+FIREBASE_ADMIN_CERT = os.getenv("FIREBASE_ADMIN_CERT")
+FIREBASE_STORAGE_LINK = os.getenv("STORAGE_LINK")
+HUGGINGFACE_API_KEY = os.getenv("HF_API_KEY")
