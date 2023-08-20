5db2c2a27788887116cfdf184e4d5a2dc82ab71c
seokkuuu <seokkub@gmail.com>
2023-08-20 15:27:57 -0400

    Implemented comparison and poll post to make post

--- src/utils/make_post.py
+++ src/utils/make_post.py
@@ -1,8 +1,12 @@
-from src.utils.create_boomer_post import create_boomer_post
+import random
 
-"""
-TODO: make the bot create other post themes
-"""
+from src.utils.create_boomer_post import create_boomer_post
+from src.utils.create_comparison_post import create_comparison_post
+from src.utils.create_poll_post import create_poll_post
 
 def make_post():
-    create_boomer_post()
+    post_themes = [create_boomer_post, create_comparison_post, create_poll_post]
+
+    create_post = random.choice(post_themes)
+
+    create_post()
